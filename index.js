cb977864a558e9b044f11870f49e9f439b907d9d
Hanzla2341 <hanzlasabir006@gmail.com>
2025-10-29 12:05:13 -0700

    third

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
+// /api/index.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('../config/db');
 const userRoutes = require('../routers/UserRouter');
 
-// Load environment variables from .env
 dotenv.config();
 
-// Create Express app
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Connect to MongoDB
 connectDB();
-
-// Routes
 app.use('/api', userRoutes);
 
-app.get('/', (req, res) => res.send('API is running...'));
-
-// Export as Vercel serverless function
-module.exports = app;
+// Instead of module.exports = app:
+const serverless = require('serverless-http');
+module.exports = serverless(app);
